36d740e3cba600efb7cf4ab09c4e03ecd55a9fbf
Aaron Newton <git@clientcide.com>
2011-04-17 20:44:39 -0700

    adding support for MooTools request tests.

--- test_runner/views.py
+++ test_runner/views.py
@@ -81,6 +81,38 @@ def read_asset(path):
   raise Exception("Unknown asset type (currently only html/png/gif/jpg/css/js are supported).")
 
 
+def mootools_request_php(req):
+  content_types = {
+    'text': 'text/plain',
+    'html': 'text/html',
+    'xml': 'application/xml',
+    'json': 'application/json',
+    'script': 'application/javascript',
+    'javascript': 'application/javascript'
+  }
+  sleep = req.REQUEST.get('__sleep')
+  response = req.REQUEST.get('__response')
+  reqtype = req.REQUEST.get('__type', 'html')
+  retrieve = req.REQUEST.get('__retrieve')
+  if sleep is not None:
+    time.sleep(float(sleep))
+
+  if response is not None:
+    return HttpResponse(response, mimetype=content_types[reqtype])
+  elif retrieve is not None:
+    response = simplejson.dumps(simplejson.loads(retrieve))
+    return HttpResponse(response, mimetype=content_types[reqtype]);
+  else:
+    obj = {
+      'method': req.META['REQUEST_METHOD'].lower()
+    }
+    if obj['method'] == 'get':
+      obj['get'] = simplejson.loads(simplejson.dumps(req.GET))
+    elif obj['method'] == 'post':
+      obj['post'] = simplejson.loads(simplejson.dumps(req.POST))
+    response = simplejson.dumps(obj)
+    return HttpResponse(response, mimetype=content_types[reqtype])
+
 # the following ajax responses borrowed from MooShell - thx Piotr!
 def echo_js(req):
     " respond JS from GET/POST['js']"
